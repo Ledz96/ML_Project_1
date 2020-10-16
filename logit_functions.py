5744fb8a7da563d056e48f2c4d2dc87484e7d919
sophieepfl <61900612+sophieepfl@users.noreply.github.com>
2020-10-16 21:23:23 +0200

    Add files via upload
    notebook with CV -logit needs debugging

---
+++ logit_functions.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""logit functions"""
+import numpy as np
+
+def sigmoid(t):
+    """apply the sigmoid function on t."""
+    z = 1/(1 + np.exp(-t))
+    return z
+
+def calculate_loss(y, tx, w):
+    """compute the loss: negative log likelihood."""
+    L=np.sum(np.log(1+np.exp(tx@w)) - y*tx@w)
+    return L
+
+def calculate_gradient(y, tx, w):
+    """compute the gradient of loss."""
+    grad=tx.T@(sigmoid(tx@w)-y)
+    return grad
+
+def learning_by_gradient_descent(y, tx, w, gamma):
+    """
+    Do one step of gradient descent using logistic regression.
+    Return the loss and the updated w.
+    """
+
+    loss=calculate_loss(y,tx,w)
+    grad=calculate_gradient(y,tx,w)
+    w=w-gamma*calculate_gradient(y,tx,w)
+
+    return loss, w
+
+def logistic_regression(y,tx,initials_w,max_iters,gamma):
+    w=initials_w
+    losses = []
+    threshold = 1e-8
+    
+    for i in range(max_iters):
+        loss, w= learning_by_gradient_descent(y, tx, w, gamma)
+        losses.append(loss)
+        if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
+            break
+    return w, loss
+
+def logistic_accuracy(y, tx, w):
+    t=tx@w
+    n=y.shape[0]
+    proba=sigmoid(t)
+    ypred=(proba>0.5).astype(int)
+    acc=(y==ypred).sum()/n
+    
+    return acc
+        
