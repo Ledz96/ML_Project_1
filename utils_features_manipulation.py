f58eb679656c0aadbb6e7c9418c267c75be131b3
Sophie De Becker <sophie.debecker@epfl.ch>
2020-10-22 09:57:11 +0200

    hello added build_poly_index

--- utils_features_manipulation.py
+++ utils_features_manipulation.py
@@ -30,6 +30,24 @@ def build_poly_multi(tx, degree):
         
     return np.array(xmat)
 
+def build_poly_index(tx, index_list, degree):
+    """polynomial basis functions for input data tx, for all features expanded to d degree.Extended matrix is inputed np.array. Index list includes all variables that will be expanded, mostly useful to avoid expanding categorical variables"""
+    # ***************************************************
+    # augmented x matrix with column of 1 input
+    #list of indices for features modified
+    #degrees of polynomial to apply to all features irrespectively
+    # ***************************************************
+    xmat=np.ones(tx.shape[0]).reshape(-1,1)
+    for i in range(1,tx.shape[1]):
+        if i in index_list:
+            for d in range(1,degree+1):
+                coltmp=tx[:,i]**d
+                xmat = np.append(xmat, coltmp.reshape(-1,1), axis=1)
+        if i not in index_list:
+            coltmp=tx[:,i]
+            xmat = np.append(xmat, coltmp.reshape(-1,1), axis=1)
+    return np.array(xmat)
+
 def min_max_scale(x):
     """Compute min-max scaling on x and return the scaled matrix"""
     
